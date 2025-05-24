2c7e8cac6705eea3c53fcd5a792721ff0426275b
AbderrahimeEl <elmoutaouakil12345@gmail.com>
2025-05-24 19:40:49 +0100

    feat: add CORS configuration for API endpoints

---
+++ src/main/java/com/elmo/digitalbanking/Controllers/WebConfig.java
@@ -0,0 +1,22 @@
+package com.elmo.digitalbanking.Controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200") // URL de ton frontend Angular
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
